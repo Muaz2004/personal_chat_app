e1472d9c82224760469ba5a69a1c8fbffca3e92d
Muaz2004 <muazindris64@gmail.com>
2025-10-30 06:26:45 +0300

    styling started

--- personalchat/admin.py
+++ personalchat/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile, Message, Group, GroupMessage
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Message)
+admin.site.register(Group)
+admin.site.register(GroupMessage)

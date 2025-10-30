2c4ab80e25d9740097dbe25f49b4e3ca115c88fe
Muaz2004 <muazindris64@gmail.com>
2025-10-30 21:57:47 +0300

    frontend altered

--- personalchatproject/urls.py
+++ personalchatproject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('personalchat.urls')), 
+    path('', include('personalchat.urls')), 
 ]
